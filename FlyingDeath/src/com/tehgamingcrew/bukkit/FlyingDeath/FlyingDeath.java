08bb6abaff67093bff28afeaa86f7ff99f4bea96
Omegahhh <fire_eagle666@hotmail.com>
2012-12-13 13:31:08 +0100

    First Commit

---
+++ FlyingDeath/src/com/tehgamingcrew/bukkit/FlyingDeath/FlyingDeath.java
@@ -0,0 +1,15 @@
+package com.tehgamingcrew.bukkit.FlyingDeath;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FlyingDeath extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+
+	public void onDisable() {
+		
+	}
+	
+}
